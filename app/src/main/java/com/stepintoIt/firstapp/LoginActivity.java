57b84bb5fd59b70c00b79bc9f9a4c8fe67f3d046
PranshuVyas <38761690+PranshuVyas@users.noreply.github.com>
2019-01-12 13:30:28 -0500

    Async Task added

--- app/src/main/java/com/stepintoIt/firstapp/LoginActivity.java
+++ app/src/main/java/com/stepintoIt/firstapp/LoginActivity.java
@@ -2,11 +2,15 @@ package com.stepintoIt.firstapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -21,6 +25,10 @@ public class LoginActivity extends AppCompatActivity {
     EditText etxPassword;
     @BindView(R.id.btn_submit)
     Button btnSubmit;
+    @BindView(R.id.txt_count)
+    TextView txtCount;
+    @BindView(R.id.pb_login)
+    ProgressBar pbLogin;
 
 
     private SharedPreferences sharedPreferences;
@@ -38,18 +46,76 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_submit)
     public void logIn() {
-        if (etxUserName.getText().toString().equals("admin")
-                && etxPassword.getText().toString().equals("1234")) {
-            Toast.makeText(LoginActivity.this, "sucess", Toast.LENGTH_LONG).show();
-            SharedPreferences.Editor preferedEditor = sharedPreferences.edit();
-            preferedEditor.putString(KEY_USERNAME, etxUserName.getText().toString());
-            preferedEditor.apply();
-            Intent mainIntent = new Intent(LoginActivity.this, DashboardActivity.class);
-            startActivity(mainIntent);
-            finish();
-        } else {
-            Toast.makeText(LoginActivity.this, "fail", Toast.LENGTH_LONG).show();
+        new LogingAsyncTask().execute(etxUserName.getText().toString(), etxPassword.getText().toString());
+
+
+    }
+
+    private class LogingAsyncTask extends AsyncTask<String, Integer, Boolean>{
+
+        @Override
+        protected void onPreExecute() {
+            pbLogin.setVisibility(View.VISIBLE);
+            txtCount.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            String userName = strings[0];
+            String passWord = strings[1];
+
+            for (int i = 0; i <5 ; i++) {
+
+                try {
+                    Thread.sleep(1000);
+                    onProgressUpdate(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (etxUserName.getText().toString().equals("admin")
+                    && etxPassword.getText().toString().equals("1234")){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(final Integer... values) {
+            super.onProgressUpdate(values);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    txtCount.setText("count = " + values[0]);
+                }
+            });
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if (aBoolean == true) {
+                Toast.makeText(LoginActivity.this, "sucess", Toast.LENGTH_LONG).show();
+                SharedPreferences.Editor preferedEditor = sharedPreferences.edit();
+                preferedEditor.putString(KEY_USERNAME, etxUserName.getText().toString());
+                preferedEditor.apply();
+                Intent mainIntent = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(mainIntent);
+                finish();
+            } else {
+                Toast.makeText(LoginActivity.this, "fail", Toast.LENGTH_LONG).show();
+            }
+
+            pbLogin.setVisibility(View.GONE);
+            txtCount.setVisibility(View.GONE);
         }
     }
+
+
+
 
 }
